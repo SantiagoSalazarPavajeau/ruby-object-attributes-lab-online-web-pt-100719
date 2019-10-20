8eb2c3da03194d2d10c6a10ec5577fef70fa94e1
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-20 20:32:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def name=(name)
+    @name=name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breeds(breed)
+    @breed=breed
+  end
+  
+  def breeds
+    @breed
+  end
+  
+end
+  
